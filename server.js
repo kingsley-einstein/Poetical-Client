23518df0ee2eaf74f4c9937957ca862f3230b415
Kingsley Victor <javaprodigy56@gmail.com>
2019-01-07 21:46:33 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/dist/Poetical')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/Poetical/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running');
+});
